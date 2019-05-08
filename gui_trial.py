a78e7004af6331ce45f954bbb05e99da82922727
schka4 <32438369+schka4@users.noreply.github.com>
2019-05-08 10:32:55 +0200

    improved gui

--- gui_trial.py
+++ gui_trial.py
@@ -4,174 +4,103 @@ Created on Monday Apr 29 20:10:02 2019
 
 GUI implementation for search algorithm comparer
 """
-"""Copy from BFS_and_DFS.py"""
-import DataRepresentation as dr #dr stands for data representation
-import ProfTrial
-p=dr.Problem()
-lst=p.readProblem("Problem.xlsx")
 
-def goal_test(s):
-    goal_name=lst[len(lst)-1].name
-    if s.name==goal_name:
-        return True
-    else:
-        return False
+import BFS_and_DFS as alg
 
-memoryLimit = 2000
-maxSteps = 50
-# if we suppose that the name is like an ID
-def equalStates(s1, s2):
-    if s1.name==s2.name:
-        return True
-    else:
-        return False
-	
-def isMember(s, lst_of_states):
-	for i in range(len(lst_of_states)):
-		if (equalStates(lst_of_states[i], s)):
-			return True
-	return False
-
-# finding the state in all states list by name
-def findStateByName(s_name,lst_states):
-    for stored_s in lst_states:
-        if stored_s.name==s_name:
-            return stored_s
-    return dr.State('not_found')
-
-#reconstruation of the path based on the saved key - value pairs during search
-def path_backtracking(fromDict,startValue,goalState):
-    path=[]
-    path.append(goalState.name)
-    for i in range(0,len(fromDict)):
-        val=fromDict.get(path[0],None)
-        if val==None or val==startValue:
-            return path
-        else:
-            path.insert(0,val)
-    return path
-    
-#BFS
-@ProfTrial.Profile_Decoder
-def bfs(start, goalFunction,all_states):
-    #dict for backtracking the found path
-    #key: the state, value: from where we got there
-    from_dict=dict()
-    #the from value for the starting state key
-    #it helps ending the becktracking
-    from_state='START'
-    from_dict[start.name]=from_state
-    queue = [] # define an empty array
-    queue.append(start) # insert an element at the end of the array
-    visited = []
-    steps = 0 #step counter
-    #finalState = State('not_found') # for storing the finalState if found
-    while(steps<maxSteps and len(queue)>0):
-        curState = queue.pop(0)
-        #print(curState.name)
-        visited.append(curState)
-        steps=steps+1
-        if (goalFunction(curState)):
-            print('BFS in ' + str(steps) + ' steps')
-            ls=path_backtracking(from_dict,from_state,curState)
-            #return curState.name
-            return ls
-        else:
-            adjStates=[]
-            adjStates=curState.children
-            for key in adjStates:
-                actual_child=findStateByName(key,all_states)
-                #the new member should not be in the visited list and also in the queue
-                if (not isMember(actual_child,visited) and not isMember(actual_child,queue)):
-                    queue.append(actual_child)
-                    from_dict[actual_child.name]=curState.name
-    print('BFS in ' + str(steps) + ' steps')
-    return 'Not found'
-
-#DFS
-@ProfTrial.Profile_Decoder
-def dfs(start, goalFunction,all_states):
-    from_dict=dict()
-    from_state='START'
-    from_dict[start.name]=from_state
-    queue = [] # define an empty array
-    queue.append(start) # insert an element at the end of the array
-    visited = []
-    steps = 0 #step counter
-    #finalState = State('not_found') # for storing the finalState if found
-    while(steps<maxSteps and len(queue)>0):
-        curState = queue.pop(-1)
-        #print(curState.name)
-        visited.append(curState)
-        steps=steps+1
-        if (goalFunction(curState)):
-            print('DFS in ' + str(steps) + ' steps')
-            ls=path_backtracking(from_dict,from_state,curState)
-            return ls
-        else:
-            adjStates=[]
-            adjStates=curState.children
-            for key in sorted(adjStates.keys(), reverse=True):
-                actual_child=findStateByName(key,all_states)
-                if (not isMember(actual_child,visited)):
-                    queue.append(actual_child)
-                    from_dict[actual_child.name]=curState.name
-    print('DFS in ' + str(steps) + ' steps')
-    return 'Not found'
-
-"""STARTING GUI IMPLEMENTATION"""
 import tkinter as tk #library for gui
 from PIL import Image, ImageTk #library to convert image format
+from tkFileDialog   import askopenfilename #for the menu
 
-"""functions at button click"""
+"""FUNCTIONS FOR WIDGETS"""
+
 def bfs_button():
-    found_path=bfs(lst[0], goal_test,lst)
-    msg = tk.Message(root, text =found_path)
-    msg.config(anchor="nw",bg='white',width=40)
-    msg.pack()
+    '''run BFS and show results'''
+    found_path=alg.bfs()
+    msg_bfs.config(text=found_path)
+    msg_bfs.text=("BFS:"+str(found_path))
+    
     fname="graph_bfs.jpg"
-    p.plotPath(found_path,fname)
-    
-    #canvas_width = 440
-    #canvas_height = 400
-    #canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
-    #canvas.pack()
+    alg.p.plotPath(found_path,fname)    
+
     im=Image.open(fname)
-    photo=ImageTk.PhotoImage(im)
-    #canvas.create_image(20,20, anchor="nw", image=photo)
-    
-    label = tk.Label(image=photo)
-    label.image = photo # keep a reference!
-    label.pack()
+    photo_bfs=ImageTk.PhotoImage(im)   
+    label_bfs.configure(image=photo_bfs)
+    label_bfs.image = photo_bfs
+
     
 def dfs_button():
-    found_path=dfs(lst[0], goal_test,lst)
-    msg = tk.Message(root, text =found_path)
-    msg.config(anchor="nw",bg='white',width=80)
-    msg.pack()
+    '''run DFS and show results'''
+    found_path=alg.dfs()
+    
+    msg_dfs.config(text=found_path)
+    msg_dfs.text=("DFS:"+str(found_path))
+    
     fname="graph_dfs.jpg"
-    p.plotPath(found_path,fname)
-    
-    #canvas_width = 440
-    #canvas_height = 400
-    #canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
-    #canvas.pack()
-    im2=Image.open(fname)
-    photo2=ImageTk.PhotoImage(im2)
-    #canvas.create_image(20,20, anchor="nw", image=photo)
-    
-    label2 = tk.Label(image=photo2)
-    label2.image = photo2 # keep a reference!
-    label2.pack()
+    alg.p.plotPath(found_path,fname)    
 
+    im=Image.open(fname)
+    photo_dfs=ImageTk.PhotoImage(im)   
+    label_dfs.configure(image=photo_dfs)
+    label_dfs.image = photo_dfs
 
-"""creating and running gui""" 
+def OpenFile():
+    '''Open files in menu'''
+    #graph_file_name = askopenfilename()
+    #lst = p.readProblem(graph_file_name)
+    fname="graph_whole.jpg"
+    alg.p.generateEdges(fname)    
+    im=Image.open(fname)
+    photo_graph=ImageTk.PhotoImage(im)
+
+    label_graph.configure(image=photo_graph)
+    label_graph.image = photo_graph
+    
+
+"""CREATING THE STRUCTURAL ELEMENTS"""
+
+''' the root, main window'''
 root = tk.Tk()
 root.title("Search algotithm comparer")
-#two buttons - they creating message and label for display the path
-button = tk.Button(root, text='BFS', width=25, command=bfs_button)
-button.pack()
-button2= tk.Button(root, text='DFS', width=25, command=dfs_button)
-button2.pack()
+root.geometry("1200x600")
+
+''' deafult panel for showing the graph'''
+msg_graph = tk.Message(root, text ="Graph:")
+msg_graph.config(anchor="nw",bg='white',width=40)
+msg_graph.grid(row=0, column=0,sticky='W')
+im=Image.open('blank.jpg')
+photo=ImageTk.PhotoImage(im)
+label_graph = tk.Label(image=photo,anchor='nw')
+label_graph.grid(row=1, column=0)
+
+''' default message box and panel for the found paths'''
+msg_bfs = tk.Message(root, text ="BFS:")
+msg_bfs.config(anchor="nw",bg='white',width=40)
+msg_bfs.grid(row=0, column=1)
+label_bfs = tk.Label(image=photo, anchor='e')
+label_bfs.image = photo
+label_bfs.grid(row=1, column=1)
+
+msg_dfs = tk.Message(root, text ="DFS:")
+msg_dfs.config(anchor="nw",bg='white',width=40)
+msg_dfs.grid(row=0, column=2)
+label_dfs = tk.Label(image=photo, anchor='e')
+label_dfs.image = photo
+label_dfs.grid(row=1, column=2)
+
+'''two buttons for displaying the result of the algorithms'''
+#- they creating message and label for display the path
+button_bfs = tk.Button(root, text='BFS', width=25, command=bfs_button)
+button_bfs.grid(row=2, column=0)
+button_dfs= tk.Button(root, text='DFS', width=25, command=dfs_button)
+button_dfs.grid(row=3, column=0)
+
+'''Menu for opening files'''
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+filemenu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="Open...", command=OpenFile)
+
 
 root.mainloop()
