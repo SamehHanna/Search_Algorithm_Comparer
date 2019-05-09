3d72db0c2aca631897c7e493f45309cad57b4b8f
schka4 <32438369+schka4@users.noreply.github.com>
2019-05-09 23:18:00 +0200

    Comments got cleared

--- BFS_and_DFS.py
+++ BFS_and_DFS.py
@@ -3,37 +3,45 @@
 """
 Created on Mon Mar 18 21:46:02 2019
 
-Contains BFS and DFS algorithms
+Contains BFS and DFS algorithms in a function, which also can run them 
+and return their results.
 
-@author: schka
+@author: Katalin Schaffer
 """
 
 import DataRepresentation as dr #dr stands for data representation
 import ProfTrial
 
 def algorithm_caller(problem_fname, alg_number,fname_outplot):
-    """if alg_number=1, then runs BFS, if 2 then run DFS, if 3 then show graph"""
-    """problem_fname: name of the excel file representing the graph"""
-    """fname_outplot: name of the output image file about the path"""
+    """
+    problem_fname: name of the excel file representing the graph
+    
+    There are 3 options of returned results (choosed by giving alg_numb=1/2/3)
+    if alg_number=1, then runs BFS and create the image about the path
+    if 2 then run DFS and create the image about the path
+    if 3 then create the image about the whole graph
+    
+    (the name of the created file is given as fname_outplot input argument)
+    """
+    """Reading the problem file"""
     p=dr.Problem()
     lst=p.readProblem(problem_fname)
-    #p.showProblem()
+    """if 3 then create the image about the whole graph and return"""
     if (alg_number==3):
         p.generateEdges(fname_outplot)
         return
-    #implementation of BFS
-         
+    
+    """IMPLEMENTATION OF THE ALGORITHMS"""
+    maxSteps = 50 # for preventing to run too much time e.g. stucked in loop
+    """Used helper functions"""
     def goal_test(s):
         goal_name=lst[len(lst)-1].name
         if s.name==goal_name:
             return True
         else:
             return False
-    
-    memoryLimit = 2000
-    maxSteps = 50
-    # if we suppose that the name is like an ID
-    def equalStates(s1, s2):
+        
+    def equalStates(s1, s2): # if we suppose that the name is like an ID
         if s1.name==s2.name:
             return True
         else:
@@ -45,14 +53,15 @@ def algorithm_caller(problem_fname, alg_number,fname_outplot):
     			return True
     	return False
     
-    # finding the state in all states list by name
+    '''finding the state in all states list by name'''
     def findStateByName(s_name,lst_states):
         for stored_s in lst_states:
             if stored_s.name==s_name:
                 return stored_s
         return dr.State('not_found')
     
-    #reconstruation of the path based on the saved key - value pairs during search
+    '''reconstruation of the path based on the saved key - value pairs
+        during search'''
     def path_backtracking(fromDict,startValue,goalState):
         path=[]
         path.append(goalState.name)
@@ -64,53 +73,49 @@ def algorithm_caller(problem_fname, alg_number,fname_outplot):
                 path.insert(0,val)
         return path
         
-    #BFS
+    """BFS"""
     @ProfTrial.Profile_Decoder
-    def bfs(start=lst[0], goalFunction=goal_test,all_states=lst):
-        #dict for backtracking the found path
-        #key: the state, value: from where we got there
+    def bfs(start=lst[0], goalFunction=goal_test,all_states=lst,printres=False):
+        '''dict for backtracking the found path'''
+        '''key: the state, value: from where we got there'''
         from_dict=dict()
-        #the from value for the starting state key
-        #it helps ending the becktracking
+        '''the from value for the starting state key, it helps ending the becktracking'''
         from_state='START'
         from_dict[start.name]=from_state
         queue = [] # define an empty array
         queue.append(start) # insert an element at the end of the array
         visited = []
         steps = 0 #step counter
-        #finalState = State('not_found') # for storing the finalState if found
         while(steps<maxSteps and len(queue)>0):
             curState = queue.pop(0)
-            #print(curState.name)
             visited.append(curState)
             steps=steps+1
             if (goalFunction(curState)):
-                print('BFS in ' + str(steps) + ' steps')
+                if printres : print('BFS in ' + str(steps) + ' steps')
                 ls=path_backtracking(from_dict,from_state,curState)
-                #return curState.name
                 return ls
             else:
                 adjStates=[]
                 adjStates=curState.children
                 for key in adjStates:
                     actual_child=findStateByName(key,all_states)
-                    #the new member should not be in the visited list and also in the queue
+                    '''the new member should not be in the visited list and also in the queue'''
                     if (not isMember(actual_child,visited) and not isMember(actual_child,queue)):
                         queue.append(actual_child)
                         from_dict[actual_child.name]=curState.name
-        print('BFS in ' + str(steps) + ' steps')
+        if printres : print('BFS in ' + str(steps) + ' steps')
         return 'Not found'
     
-    #run the search from the first state
+    """If 1st option (BFS) was choosen then run the search from the first state"""
     if (alg_number==1):
         found_path_bfs=bfs(lst[0], goal_test,lst)
         p.plotPath(found_path_bfs,fname_outplot)
-        print(found_path_bfs)
+        #print(found_path_bfs)
         return found_path_bfs
     
-    #DFS
+    """DFS"""
     @ProfTrial.Profile_Decoder
-    def dfs(start=lst[0], goalFunction=goal_test,all_states=lst):
+    def dfs(start=lst[0], goalFunction=goal_test,all_states=lst,printres=False):
         from_dict=dict()
         from_state='START'
         from_dict[start.name]=from_state
@@ -118,14 +123,12 @@ def algorithm_caller(problem_fname, alg_number,fname_outplot):
         queue.append(start) # insert an element at the end of the array
         visited = []
         steps = 0 #step counter
-        #finalState = State('not_found') # for storing the finalState if found
         while(steps<maxSteps and len(queue)>0):
             curState = queue.pop(-1)
-            #print(curState.name)
             visited.append(curState)
             steps=steps+1
             if (goalFunction(curState)):
-                print('DFS in ' + str(steps) + ' steps')
+                if printres : print('DFS in ' + str(steps) + ' steps')
                 ls=path_backtracking(from_dict,from_state,curState)
                 return ls
             else:
@@ -136,17 +139,12 @@ def algorithm_caller(problem_fname, alg_number,fname_outplot):
                     if (not isMember(actual_child,visited)):
                         queue.append(actual_child)
                         from_dict[actual_child.name]=curState.name
-        print('DFS in ' + str(steps) + ' steps')
+        if printres : print('DFS in ' + str(steps) + ' steps')
         return 'Not found'
     
-    #run the search from the first state
+    """If 2nd option (DFS) was choosen then run the search from the first state"""
     if (alg_number==2):
         found_path_dfs=dfs(lst[0], goal_test,lst)
         p.plotPath(found_path_dfs,fname_outplot)
-        print(found_path_dfs)
+        #print(found_path_dfs)
         return found_path_dfs
-'''
-p.generateEdges()
-p.plotPath(found_path,"pb.jpg")
-p.plotPath(found_path_dfs,"pd.jpg")
-'''
